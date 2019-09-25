d702affc92156d32a2ff3388d52402b5e84d8bb7
2003Robi <47587946+2003Robi@users.noreply.github.com>
2019-09-25 10:19:15 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int b=0,n,i,a,S=0,c,x,y,d;
+     int b=0,c,n,i,a,S=0;
     cout <<"n= ";
     cin >>n;
     for(i=0; i<n; i++)
@@ -12,27 +12,11 @@ int main()
         cout<<"a= ";
         cin >>a;
         if (a%2==0)
-        {
-
-            S=S+a;
-            b=b+1;
-        }
-
-        c=S/b;
-        cout<<"paros szamok atlaga "<<c<<endl;
-
-        else
-        {
-            x=x+a;
-            y=y+1;
-        }
+        S=S+a;
+        b=b+1;
     }
-
-            d=x/y;
-
-            cout<<"paratlan szamok atlaga "<<d<<endl;
-
-
+        c=S/b;
+        cout<<"atlag "<<c<<endl;
 
 
 
